2fd781df4b241646826fff482d3f462282d8e9df
jluc9234 <dogpoundreject1@gmail.com>
2026-02-17 02:57:53 -0700

    Update Supabase anon key in supabaseClient.ts
    Updated the Supabase anon key for the client.

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 // Use the URL from your Supabase Dashboard
 const supabaseUrl = 'https://sqbrkmileouiqeijqmyo.supabase.co';
 // Get your actual 'anon' 'public' key from Settings > API
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY'; 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxYnJrbWlsZW91aXFlaWpxbXlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMDgyNTcsImV4cCI6MjA4Njg4NDI1N30.iiO46ossD4GWPvzskktg3jBDiKHziXdRaTdz0-_oTPQ'; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
